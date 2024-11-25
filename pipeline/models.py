ca093e5cee508360ea221cc5c8ceeb33f44b8e66
SamiJuhani <sami.paananen@gmail.com>
2024-11-25 16:30:07 +0200

    update import for declarative_base. From pytest warning: MovedIn20Warning: The `declarative_base()` function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9

--- pipeline/models.py
+++ pipeline/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Date
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from .const import TABLE_NAME 
 
